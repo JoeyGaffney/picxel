98737648d56d3cbd6b3e991395ccc597b4ac1af3
Joey Gaffney <jgaffney99@gmail.com>
2020-08-25 19:16:55 -0700

    init commit
    Completed Phase 1

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # picxel
 Personal Project
+
+A simple program to convert any picture to a pixellized version.
+
+## **TODO**
+Phase 1, Basics:
+- [x] Import a picture from filepath
+- [x] Resize
+- [x] Convert to 2-D Array
+- [x] Edit each pixel in array to either black or white
+- [x] Save picture
+
+Phase 2, GUI:
+- [] Open an HTML webpage
+- [] Add button to import a photo
+- [] Display Resized photo
+- [] Add a drop down list to choose pixel resolution
+- [] Add a button to convert picture
+- [] Display new picture side-by-side
+
+Phase 3, more Features:
+- [] Implement degree of quantization option
+- [] Make compatible with .gif files
+- [] Decorate GUI, webpage accessible
+- [] Animation/visualization of file being converted
+
+
---
+++ pixel.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot
+from matplotlib import image
+import random
+###################################################
+## Import Image here
+image = Image.open('pics\cat.jpg')
+## Set the Size here
+size = 128 #px
+###################################################
+load_img_rz = np.array(image.resize((size,size)))
+Image.fromarray(np.uint8(load_img_rz)).save('r_pic.jpg')
+grey_img = np.zeros(shape = (size,size))
+threshold = 300
+bl_threshold = 150
+wh_threshold = 500
+for i in range(0,size):
+    for j in range(0,size):
+        if(sum(load_img_rz[i,j]) < bl_threshold):
+            grey_img[i,j] = 0
+            if(j<size-1 and random.randint(1,16) == 2):
+                grey_img[i,j+1] = 0
+            if(i<size-1 and random.randint(1,16) == 2):
+                grey_img[i+1,j] = 0
+            if(j > 0 and random.randint(1,16) == 2):
+                grey_img[i,j-1] = 0
+        elif(sum(load_img_rz[i,j]) > wh_threshold):
+            grey_img[i,j] = 255
+            if(j<size-1 and random.randint(1,2) == 2):
+                grey_img[i,j+1] = 255
+            if(i<size-1 and random.randint(1,2) == 2):
+                grey_img[i+1,j] = 255
+            if(j>0 and random.randint(1,2) == 2):
+                grey_img[i,j-1] = 255
+        elif(sum(load_img_rz[i,j]) < threshold and grey_img[i,j] != 255):
+            grey_img[i,j] = 0
+            if(j < size-1 and random.randint(1,4) == 2):
+                grey_img[i,j+1] = 255
+            if(i < size-1 and random.randint(1,4) == 2):
+                grey_img[i+1,j] = 255
+                if(j < size-1 and random.randint(1,4) == 2):
+                    grey_img[i+1,j+1] = 255
+                if(j > 0 and random.randint(1,4) == 2):
+                    grey_img[i+1,j-1] = 255
+        elif(sum(load_img_rz[i,j]) >= threshold):
+            grey_img[i,j] = 255
+        
+Image.fromarray(np.uint8(grey_img)).save('bw_pic.jpg')
